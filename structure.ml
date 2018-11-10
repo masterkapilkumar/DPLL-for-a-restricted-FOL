c14518fe5aca1dd1a15cb2edb66b76daecff90c4
Kapil Kumar <masterkapilkumar@gmail.com>
2018-11-11 02:17:00 +0530

    well-formed checker done

--- structure.ml
+++ structure.ml
@@ -1,7 +1,7 @@
 open Signature
 open Printf
 
-module Assignment3 : Assignment3 =
+module Assignment3 =
 struct
 	type term = C of string | V of string | F of string * (term list)
 	type form = PRED of string * (term list)
@@ -15,7 +15,85 @@ struct
 	exception Not_closed of term list
 	exception DPLL_unsat of int
 	
-	let wff formula = printf "dcshdshds"; true
+	(* Remove duplicates formulae or terms *)
+	let remove_duplicates form_list =
+		let rec remove_dups all_forms forms_set =
+			match all_forms with
+				 f::tl -> (try let _ = (List.find (fun x->x=f) forms_set) in remove_dups tl forms_set
+						  with Not_found -> remove_dups tl (f::forms_set)
+						  )
+				|[] -> forms_set
+		in
+			remove_dups form_list []
+	
+	let wff formula =
+		let rec find_terms_in_term term =
+			match term with
+				 C(s)-> []
+				|V(s)-> []
+				|F(s,termsl)-> term::(List.flatten(List.map find_terms_in_term termsl))
+		in		
+		let rec find_terms_in_formula form =
+			match form with
+				 PRED(s, termsl) -> List.flatten(List.map find_terms_in_term termsl)
+				|NOT(f) -> find_terms_in_formula f
+				|AND(f1,f2) -> (find_terms_in_formula f1)@(find_terms_in_formula f2)
+				|OR(f1,f2) -> (find_terms_in_formula f1)@(find_terms_in_formula f2)
+				|FORALL(t,f) -> find_terms_in_formula f
+				|EXISTS(t,f) -> find_terms_in_formula f
+		in
+		let rec find_formula form =
+			match form with
+				 PRED(s, termsl) -> [form]
+				|NOT(f) -> find_formula f
+				|AND(f1,f2) -> (find_formula f1)@(find_formula f2)
+				|OR(f1,f2) -> (find_formula f1)@(find_formula f2)
+				|FORALL(t,f) -> find_formula f     (*TODO: qualifier binds only one variable*)
+				|EXISTS(t,f) -> find_formula f
+		in
+		let rec check_formula_arities forms invalid =
+			match forms with
+				 f::tl -> (match f with
+							(* If s1 is already in the list of invalid formulas then continue else find whether s1 is invalid or not *)
+							 PRED(s1,l1) -> (try let _ = (List.find (fun x->match x with PRED(s,l)->s=s1 | _->false) invalid) in check_formula_arities tl invalid
+											  with Not_found ->
+												(* Find all the formulas matching with predicate s1 *)
+												let inv = List.filter (fun x->match x with PRED(s,l)->s=s1 | _->false) forms
+												(* Check if s1 has multiple arities *)
+											    in if (List.length (List.filter (fun x->match x with PRED(s,l)->(List.length l) <> (List.length l1) | _->false) inv)) > 0 
+												   then check_formula_arities tl (inv@invalid)
+												   else check_formula_arities tl invalid
+											  )
+							|_-> check_formula_arities tl invalid
+						  )
+				|[] -> invalid
+		in
+		let rec check_term_arities terms invalid =
+			match terms with
+				 t::tl -> (match t with
+							(* If s1 is already in the list of invalid terms then continue else find whether s1 is invalid or not *)
+							 F(s1,l1) -> (try let _ = (List.find (fun x->match x with F(s,l)->s=s1 | _->false) invalid) in check_term_arities tl invalid
+											  with Not_found ->
+												(* Find all the terms matching with term s1 *)
+												let inv = List.filter (fun x->match x with F(s,l)->s=s1 | _->false) terms
+												(* Check if s1 has multiple arities *)
+												in if List.length (List.filter (fun x->match x with F(s,l)->(List.length l) <> (List.length l1) | _->false) inv) > 0
+												   then check_term_arities tl (inv@invalid)
+												   else check_term_arities tl invalid
+											  )
+							|_-> check_term_arities tl invalid
+						  )
+				|[] -> invalid
+		in
+			let all_formulas = remove_duplicates (find_formula formula) in
+			let all_terms = remove_duplicates (find_terms_in_formula formula) in
+			let all_invalid_formulas = check_formula_arities all_formulas [] in
+			let all_invalid_terms = check_term_arities all_terms [] in
+				if(all_invalid_formulas=[] && all_invalid_terms=[]) then
+					true
+				else let _ = raise (Not_wff(all_invalid_terms, all_invalid_formulas)) in false
+	
+	
 	let fv formula = [V("a")]
 	let closed formula = true
 	let scnf formula = formula
@@ -23,3 +101,9 @@ struct
 	let sat formula n = (true, [V("a")], [PRED("aa",[V("a")])])
 	
 end;;
+(*
+let t1 = Assignment3.C "a";;
+let fml = Assignment3.AND(Assignment3.PRED("p", [Assignment3.F("f",[t1]); Assignment3.F("f",[t1]); t1]), Assignment3.NOT(Assignment3.PRED("p",[t1;t1;t1])));;
+Assignment3.wff fml;;
+
+*)
--- testcase.ml
+++ testcase.ml
@@ -3,4 +3,5 @@ open Structure.Assignment3
 let t1 = C "romeo";;
 let t2 = C "juliet";;
 let fml = PRED("loves", [t1; t2]);;
-wff fml;;
+let res = wff fml;;
+let _ = if res then Printf.printf "Formula is well-formed\n" else Printf.printf "Formula is not well-formed\n"
